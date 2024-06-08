82802b481e1dcdfb206db85e629d0f8a4c9cf483
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-08 03:50:38 -0300

    Update: Footer v5

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -52,7 +52,7 @@ export default function Footer() {
       </div>
 
       <div className={`absolute z-40 left-0 right-0`}>
-        <Image className={`cursor-none mx-auto pt-80 opacity-90`} src={`/logo.svg`} width={320} height={320} alt="Comedoria Empório da Brisa" priority />
+        <Image className={`cursor-none mx-auto opacity-90`} src={`/logo.svg`} width={320} height={320} alt="Comedoria Empório da Brisa" priority />
         <div className={`flex justify-center items-center gap-6 z-40`}>
           <div className={`animate-wiggle`}>
             <span className={`text-red-500`}>
