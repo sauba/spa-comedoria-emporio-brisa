ddedacde0b9cc27bb2ca994131a376014bf5c1f7
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-09 22:43:58 -0300

    Update: About Component added tag p Cardápio text-8xl pb-32 text-own-brown-dark

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,7 +12,7 @@ export default function About() {
         text-center
         outline-none
         border-none
-        text-zinc-100
+        text-own-brown-dark
         font-caveat
       `} id="about">
       <Image src={`/logo.svg`} width={320} height={320} alt="" className={`cursor-none mx-auto hover:transition-transform hover:scale-125`} priority />
