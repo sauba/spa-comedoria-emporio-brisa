1f817195c9b8fef58dee3d6ff40eacfb7a594f97
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2024-06-11 20:35:02 -0300

    Atualizar o index.tsx

--- src/components/CardapioPasteis/index.tsx
+++ src/components/CardapioPasteis/index.tsx
@@ -22,12 +22,12 @@ export default function CardapioPasteis() {
         <SquareHalfBottom size={48} weight="duotone" className={`-rotate-6`} />
       </span>
 
-      <div className={`w-full p-4 flex justify-center items-center gap-4`}>
+      <div className={`w-full p-4 flex flex-col justify-center items-center gap-4`}>
         <span className={`w-full flex text-center justify-center xl:justify-center items-center gap-3`}>
           <span className={`text-2xl font-pt-mono`}>Escolha 03 Opções de Recheio</span>
         </span>
 
-        <div className={`flex justify-center items-center gap-3`}>
+        <div className={`flex flex-col justify-center items-center gap-3`}>
           <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
             <Circle size={16} weight="duotone" />
             <span className={`text-2xl font-pt-mono`}>Queijo</span>
