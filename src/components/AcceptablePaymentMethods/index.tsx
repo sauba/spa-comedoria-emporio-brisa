13551a4169d2bb0f00002256be91a9b2b0ba951d
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-05 14:57:58 -0300

    Update: Logo Image v4

--- src/components/AcceptablePaymentMethods/index.tsx
+++ src/components/AcceptablePaymentMethods/index.tsx
@@ -30,12 +30,12 @@ export default function AcceptablePaymentMethods() {
         <p className={`text-4xl 2xl:text-6xl font-bold`}>Cartões</p>
       </span>
 
-      <div className={`flex flex-wrap justify-center items-center gap-6 py-8`}>
-        <Image src={`/visa.svg`} width={200} height={200} alt="" />
-        <Image src={`/hipercard.svg`} width={200} height={200} alt="" />
-        <Image src={`/mastercard.svg`} width={200} height={200} alt="" />
-        <Image src={`/elocard.svg`} width={200} height={200} alt="" />
-        <Image src={`/sodexo.svg`} width={200} height={200} alt="" />
+      <div className={`flex flex-wrap justify-center items-center gap-6 mb-8 py-8`}>
+        <Image src={`/visa.svg`} width={128} height={128} alt="" />
+        <Image src={`/hipercard.svg`} width={128} height={128} alt="" />
+        <Image src={`/mastercard.svg`} width={128} height={128} alt="" />
+        <Image src={`/elocard.svg`} width={128} height={128} alt="" />
+        <Image src={`/sodexo.svg`} width={128} height={128} alt="" />
       </div>
     </div>
   )
