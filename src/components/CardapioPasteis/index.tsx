64152c92def585e448b3ee6bed54856b3fa2de0e
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-27 13:08:44 -0300

    Update: preços de cardapio v8

--- src/components/CardapioPasteis/index.tsx
+++ src/components/CardapioPasteis/index.tsx
@@ -33,12 +33,12 @@ export default function CardapioPasteis() {
         <div className={`grid grid-cols-2 justify-center items-center gap-3`}>
           <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
             <Circle size={16} weight="duotone" />
-            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Queijo</span>
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Calabresa</span>
           </span>
 
           <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
             <Circle size={16} weight="duotone" />
-            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Calabresa</span>
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Charque</span>
           </span>
 
           <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
@@ -48,11 +48,59 @@ export default function CardapioPasteis() {
 
           <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
             <Circle size={16} weight="duotone" />
-            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Charque</span>
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Queijo Coalho</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Queijo Mussarela</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Presunto</span>
           </span>
         </div>
+
+        <span className={`w-full flex text-center justify-center xl:justify-center items-center gap-3`}>
+          <span className={`text-3xl 2xl:text-6xl`}>Acompanhamentos</span>
+        </span>
+
+        <div className={`grid grid-cols-2 justify-center items-center gap-3`}>
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Cebola</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Tomate</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Repolho</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Ervilha</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Milho</span>
+          </span>
+
+          <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
+            <Circle size={16} weight="duotone" />
+            <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-pt-mono`}>Ovo de Codorna</span>
+          </span>
+        </div>
+
         <span className={`w-full flex text-center justify-center xl:justify-center items-center`}>
-          <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl p-1 font-pt-mono font-bold rounded-lg bg-own-brown text-zinc-50`}>R$ #,##</span>
+          <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl p-1 font-pt-mono font-bold rounded-lg bg-own-brown text-zinc-50`}>01 Recheio: R$ #,##</span>
+          <span className={`text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl p-1 font-pt-mono font-bold rounded-lg bg-own-brown text-zinc-50`}>02 Recheio: R$ #,##</span>
         </span>
       </div>
     </div>
